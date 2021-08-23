03476adaf183083f303880b4676b696720130dac
funkyduck <e@m.ail>
2021-08-23 04:58:56 -0400

    fixes hazardous conditions updates, though what this looks like live still remains to be determined

--- lib/feed.py
+++ lib/feed.py
@@ -1,25 +1,41 @@
-import logging
 import json
 from typing import List
 import datetime
 import feedgenerator
 from lib import datatypes, db
 
-def _forecast(forecasts: List[datatypes.DailyForecast]) -> str:
+def gen_forecast(forecasts: List[datatypes.DailyForecast]) -> str:
     daily_forecasts: List[str] = []
     for each in forecasts:
         daily_forecasts.append(f'''<b>{each["period"]}</b>: {each["short_description"]}. {each["temperature"]}
 <br/>
 <p><i>{each["description"].replace(each["period"] + ":", "")}</i></p><hr/>''')
-    return "<br/>".join(daily_forecasts)
+    return "<br/>".join(daily_forecasts) or 'None'
 
-def _hazardous_conditions(conditions: List[datatypes.HazardousConditions]) -> str:
-    hazards = []
-    for hazard in conditions:
-        hazards.append(f'<a href={hazard["details_link"]}>{hazard["condition"]}</a>')
-    return "<br/>".join(hazards) or "None"
+def gen_hazardous_conditions(condition_list: List[datatypes.HazardousConditions], old_condition_list: List[datatypes.HazardousConditions]) -> str:
+    old_conditions = [
+        hazard['condition']
+        for hazard in old_condition_list
+    ]
 
-def _news(items: List[datatypes.ForecastNews]):
+    current_conditions = [
+        hazard['condition']
+        for hazard in condition_list
+    ]
+
+    current_hazards = [
+        f'{condition} - LIFTED!'
+        for condition in old_conditions
+        if condition not in current_conditions
+    ]
+    current_hazards.extend([
+        f'<a href={hazard["details_link"]}>{hazard["condition"]}{" - NEW!" if hazard["condition"] not in old_conditions else ""}</a>'
+        for hazard in condition_list
+    ])
+
+    return "<br/>".join(current_hazards) or "None"
+
+def gen_news(items: List[datatypes.ForecastNews]):
     news_items = []
     for item in items:
         news_items.append(f'''
@@ -35,32 +51,21 @@ def generate_feed(forecast: datatypes.FullForecast, location: str) -> str:
         description='Weather forecasts, backed by data lovingly scraped from NOAA',
         language='en',
     )
-    hazardous_conditions = _hazardous_conditions(forecast['hazardous_conditions'])
-
     cached_forecast = db.lookup_cached_forecast(location)
 
     # only publish a new post if the forecast's "last_updated" field has changed
     if cached_forecast and cached_forecast['forecast']['last_updated'] == forecast['last_updated']:
         return cached_forecast['feed_XML']
 
-    if cached_forecast and cached_forecast['forecast']['hazardous_conditions'] != forecast['hazardous_conditions']:
-        feed.add_item(
-            title=forecast['forecast']['title'] + ' - Hazardous Conditions Updated!',
-            pubdate=now,
-            link=forecast['url'],
-            unique_id=forecast['hazardous_conditions'],
-            description='Hazardous Conditions Advisory Update',
-            content=f'''<h1>Current Hazardous Conditions Advisories</h1>
-    <p>{forecast['hazardous_conditions']}</p>
-    <br/>
-    <h1>Previous Hazardous Conditions Advisories</h1>
-    <p>{cached_forecast["forecast"]["hazardous_conditions"]}</p>
-    <br/>
-    ''')
+    old_conditions = cached_forecast['forecast']['hazardous_conditions'] if cached_forecast else []
+    hazardous_conditions = gen_hazardous_conditions(
+        forecast['hazardous_conditions'],
+        old_conditions
+    )
 
-    news = _news(forecast['news'])
+    news = gen_news(forecast['news'])
 
-    full_forecast = _forecast(forecast['forecast']['daily_forecasts'])
+    full_forecast = gen_forecast(forecast['forecast']['daily_forecasts'])
 
     current_hazards = "<br/>".join(forecast['forecast']['current_hazards']) or "None"
 
